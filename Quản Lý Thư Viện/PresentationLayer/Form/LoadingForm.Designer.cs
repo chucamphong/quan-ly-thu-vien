bb36ad56c1d85f85129acadc720dfa45d685915d
Chu Cẩm Phong <chucamphong1999@gmail.com>
2019-12-03 21:27:47 +0700

    Chỉnh sửa tên của các control trong Loading Form

--- Quản Lý Thư Viện/PresentationLayer/Form/LoadingForm.Designer.cs
+++ Quản Lý Thư Viện/PresentationLayer/Form/LoadingForm.Designer.cs
@@ -29,50 +29,50 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.gunaControlBox1 = new Guna.UI.WinForms.GunaControlBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnClose = new Guna.UI.WinForms.GunaControlBox();
+            this.pnlLayout = new System.Windows.Forms.TableLayoutPanel();
             this.prgLoading = new Guna.UI.WinForms.GunaProgressBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblCopyRight = new Guna.UI.WinForms.GunaLabel();
             this.tmrLoading = new System.Windows.Forms.Timer(this.components);
-            this.tableLayoutPanel1.SuspendLayout();
+            this.pnlLayout.SuspendLayout();
             this.SuspendLayout();
             // 
-            // gunaControlBox1
+            // btnClose
             // 
-            this.gunaControlBox1.AnimationHoverSpeed = 0.07F;
-            this.gunaControlBox1.AnimationSpeed = 0.03F;
-            this.gunaControlBox1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.gunaControlBox1.IconColor = System.Drawing.Color.LightGray;
-            this.gunaControlBox1.IconSize = 15F;
-            this.gunaControlBox1.Location = new System.Drawing.Point(736, 3);
-            this.gunaControlBox1.Name = "gunaControlBox1";
-            this.gunaControlBox1.OnHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(161)))), ((int)(((byte)(156)))));
-            this.gunaControlBox1.OnHoverIconColor = System.Drawing.Color.White;
-            this.gunaControlBox1.OnPressedColor = System.Drawing.Color.Transparent;
-            this.gunaControlBox1.OnPressedDepth = 0;
-            this.gunaControlBox1.Size = new System.Drawing.Size(45, 49);
-            this.gunaControlBox1.TabIndex = 0;
+            this.btnClose.AnimationHoverSpeed = 0.07F;
+            this.btnClose.AnimationSpeed = 0.03F;
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClose.IconColor = System.Drawing.Color.LightGray;
+            this.btnClose.IconSize = 15F;
+            this.btnClose.Location = new System.Drawing.Point(736, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.OnHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(161)))), ((int)(((byte)(156)))));
+            this.btnClose.OnHoverIconColor = System.Drawing.Color.White;
+            this.btnClose.OnPressedColor = System.Drawing.Color.Transparent;
+            this.btnClose.OnPressedDepth = 0;
+            this.btnClose.Size = new System.Drawing.Size(45, 49);
+            this.btnClose.TabIndex = 0;
             // 
-            // tableLayoutPanel1
+            // pnlLayout
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.gunaControlBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.prgLoading, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.gunaLabel1, 0, 3);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.92308F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 73.07692F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 165F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 411);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.pnlLayout.ColumnCount = 1;
+            this.pnlLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.pnlLayout.Controls.Add(this.btnClose, 0, 0);
+            this.pnlLayout.Controls.Add(this.prgLoading, 0, 2);
+            this.pnlLayout.Controls.Add(this.lblTitle, 0, 1);
+            this.pnlLayout.Controls.Add(this.lblCopyRight, 0, 3);
+            this.pnlLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlLayout.Location = new System.Drawing.Point(0, 0);
+            this.pnlLayout.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlLayout.Name = "pnlLayout";
+            this.pnlLayout.RowCount = 4;
+            this.pnlLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.92308F));
+            this.pnlLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 73.07692F));
+            this.pnlLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 165F));
+            this.pnlLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.pnlLayout.Size = new System.Drawing.Size(784, 411);
+            this.pnlLayout.TabIndex = 0;
             // 
             // prgLoading
             // 
@@ -89,34 +89,34 @@
             this.prgLoading.Size = new System.Drawing.Size(744, 10);
             this.prgLoading.TabIndex = 1;
             // 
-            // label1
+            // lblTitle
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label1.Location = new System.Drawing.Point(20, 147);
-            this.label1.Margin = new System.Windows.Forms.Padding(20);
-            this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
-            this.label1.Size = new System.Drawing.Size(744, 37);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Đồ Án Quản Lý Thư Viện";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblTitle.Location = new System.Drawing.Point(20, 147);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.lblTitle.Size = new System.Drawing.Size(744, 37);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Đồ Án Quản Lý Thư Viện";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // gunaLabel1
+            // lblCopyRight
             // 
-            this.gunaLabel1.AutoSize = true;
-            this.gunaLabel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.gunaLabel1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.gunaLabel1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.gunaLabel1.Location = new System.Drawing.Point(589, 369);
-            this.gunaLabel1.Name = "gunaLabel1";
-            this.gunaLabel1.Size = new System.Drawing.Size(192, 42);
-            this.gunaLabel1.TabIndex = 2;
-            this.gunaLabel1.Text = "Copyright © Cẩm Phong, Duy Anh";
-            this.gunaLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblCopyRight.AutoSize = true;
+            this.lblCopyRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lblCopyRight.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lblCopyRight.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblCopyRight.Location = new System.Drawing.Point(589, 369);
+            this.lblCopyRight.Name = "lblCopyRight";
+            this.lblCopyRight.Size = new System.Drawing.Size(192, 42);
+            this.lblCopyRight.TabIndex = 2;
+            this.lblCopyRight.Text = "Copyright © Cẩm Phong, Duy Anh";
+            this.lblCopyRight.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // tmrLoading
             // 
@@ -129,7 +129,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
             this.ClientSize = new System.Drawing.Size(784, 411);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.pnlLayout);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -139,19 +139,19 @@
             this.Name = "LoadingForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Loading Form";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
+            this.pnlLayout.ResumeLayout(false);
+            this.pnlLayout.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Guna.UI.WinForms.GunaControlBox gunaControlBox1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Guna.UI.WinForms.GunaControlBox btnClose;
+        private System.Windows.Forms.TableLayoutPanel pnlLayout;
         private Guna.UI.WinForms.GunaProgressBar prgLoading;
-        private System.Windows.Forms.Label label1;
-        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private System.Windows.Forms.Label lblTitle;
+        private Guna.UI.WinForms.GunaLabel lblCopyRight;
         private System.Windows.Forms.Timer tmrLoading;
     }
 }
