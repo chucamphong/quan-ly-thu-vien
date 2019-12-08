c7aa5fbe069a9857a3378f095aa677a9600bf1d5
Chu Cẩm Phong <chucamphong1999@gmail.com>
2019-12-08 19:01:38 +0700

    Thêm Loading Form

---
+++ Quản Lý Thư Viện/PresentationLayer/Forms/LoadingForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer.Forms
+{
+    public partial class LoadingForm : Form
+    {
+        public LoadingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadingForm_Load(object sender, EventArgs e)
+        {
+            Guna.UI.Lib.GraphicsHelper.ShadowForm(sender as LoadingForm);
+        }
+    }
+}
