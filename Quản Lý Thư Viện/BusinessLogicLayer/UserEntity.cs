69e9e3368752d2f97bd03f96de2b8ae5676b6cb3
Chu Cẩm Phong <chucamphong1999@gmail.com>
2019-12-08 18:43:49 +0700

    Sửa @email thành email

--- Quản Lý Thư Viện/BusinessLogicLayer/UserEntity.cs
+++ Quản Lý Thư Viện/BusinessLogicLayer/UserEntity.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DataAccessLayer;
-using DataTransferObject.Models;
+﻿using DataAccessLayer;
 using Helper.Crypto;
 
 namespace BusinessLogicLayer
 {
     public class UserEntity
     {
-        public static bool Login(string @email, string password)
+        public static bool Login(string email, string password)
         {
             string hashPassword = MD5.Hash(password);
 
-            return UserData.FindByEmailAndPassword(@email, hashPassword) is User;
+            return UserData.FindByEmailAndPassword(email, hashPassword) != null;
         }
     }
 }
