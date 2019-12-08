9f2439d46da1a123e3a37c4f21888358be3aaf9c
Chu Cẩm Phong <chucamphong1999@gmail.com>
2019-12-08 15:57:47 +0700

    Loại bỏ các thư viện dư thừa và format lại code

--- Quản Lý Thư Viện/PresentationLayer/Forms/SplashScreen.cs
+++ Quản Lý Thư Viện/PresentationLayer/Forms/SplashScreen.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PresentationLayer.Forms
@@ -21,6 +14,5 @@ namespace PresentationLayer.Forms
         {
             Guna.UI.Lib.GraphicsHelper.ShadowForm(sender as SplashScreen);
         }
-
     }
 }
